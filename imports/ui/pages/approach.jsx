6c0a102f68b8efea3e42494729bc1f2b6829eb3b
Ben Israeli <jisraeli@gmail.com>
2016-08-20 13:58:38 -0400

    altered approach copy adding viriginia's line

--- imports/ui/pages/approach.jsx
+++ imports/ui/pages/approach.jsx
@@ -15,9 +15,9 @@ export default class Appraoch extends React.Component {
         <div className="col s12">
           <section className="content">
             <p className="content-text">
-              Mr. Israeli approaches education from many angles,
+              Ben approaches education from many angles,
               structuring sessions to advance the student on a learning track tailored
-              to his/her learning needs. Key to Ben's teaching philosophy is soliciting the student’s
+              to his/her learning needs. Key to his teaching philosophy is soliciting the student’s
               active, consistent engagement in the learning process. The goal is each
               student’s management of his/her learning.
             </p>
@@ -30,6 +30,12 @@ export default class Appraoch extends React.Component {
               When students engage in a way that helps them understand what learning can feel like, then
               my job is done.”
             </blockquote>
+
+            <p className="content-text">
+              Ben's background in teaching students with different learning styles means that he customizes
+              even his test prep to each student's needs. One size does not fit all.
+            </p>
+
 
             <p className="content-text">
               Mr. Israeli offers regular session hours, as well as weekend and online
